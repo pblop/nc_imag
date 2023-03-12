b954854e58548aa2b73d4e9c81466ae2cf7e5dea
pablo <pablo@pabl.eu>
2023-03-13 00:57:41 +0100

    basic socket stuff

--- main.c
+++ main.c
@@ -3,13 +3,40 @@
 #include "util.h"
 #include <stdio.h>
 #include <signal.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+
+struct {
+  int sockfd;
+} globals = {-1};
 
 void sigint_handler(int signum);
 void setup_sigint_handler(void);
 
+int main()
+{
+  setup_sigint_handler();
+
+  // Create the socket
+  if ((globals.sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+  {
+    perror("create socket");
+    return 1;
+  }
+  
+
+
+  return 0;
+}
+
 void sigint_handler(int signum)/*{{{*/
 {
   UNUSED(signum);
+
+  if (globals.sockfd != -1)
+    shutdown(globals.sockfd, SHUT_RDWR); // only should fail if our socket
+                                         // is already closed.
+  
 
   exit(0);
 }/*}}}*/
@@ -25,8 +52,3 @@ void setup_sigint_handler(void)/*{{{*/
   action.sa_flags = SA_RESTART;
   sigaction(SIGINT, &action, NULL);
 }/*}}}*/
-
-int main()
-{
-  return 0;
-}
