9f2ae1e04d1ca82dfad219e7c0198c17c863325a
pablo <pablo@pabl.eu>
2023-03-13 01:33:30 +0100

    Children and signal handling stuff.

--- main.c
+++ main.c
@@ -10,13 +10,20 @@
 
 struct {
   int sockfd;
-} globals = {-1};
+  int connfd;
+} globals = {-1, -1};
 
-void sigint_handler(int signum);
+// Parent signal handlers
+void parent_sigint_handler(int signum);
+void setup_parent_handler(void);
 void cleanup_exit(int exitn);
-void setup_sigint_handler(void);
 
-int main(int argc, char* argv[])
+// Children signal handlers
+void setup_child_handler(void);
+void child_sigusr1_handler(int signum);
+int connection_logic(int connfd);
+
+int main(int argc, char* argv[])/*{{{*/
 {
   int port, connfd;
   struct sockaddr_in addr;
@@ -34,7 +41,7 @@ int main(int argc, char* argv[])
   // TODO: Check if argv[1] really is a number.
   port = atoi(argv[1]);
 
-  setup_sigint_handler();
+  setup_parent_handler();
 
   // Create the socket
   if ((globals.sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
@@ -74,37 +81,101 @@ int main(int argc, char* argv[])
       perror("accept");
       cleanup_exit(1);
     }
-
-    write(connfd, "Hello, world!", 14);
-
-    close(connfd);
+    
+    // The stuff needed to print the image (prompting the terminal for the
+    // current screen size and stuff like that), will probably take some time.
+    // I'm making a new thread to handle every new incomming connection.
+    switch (fork())
+    {
+      case -1:
+        perror("fork");
+        cleanup_exit(1);
+      case 0:
+        exit(connection_logic(connfd));
+      default:
+        break;
+    }
   }
 
   return 0;
-}
+}/*}}}*/
 
-void sigint_handler(int signum)/*{{{*/
+// This is the main function for all incomming connections.
+int connection_logic(int connfd)/*{{{*/
+{
+  globals.connfd = connfd; // Save the connection file descriptor for the signal handler.
+  
+  // We must ignore sigints because they will be sent to us whenever our parent
+  // process is being killed via ^C.
+  // Our parent process will then SIGUSR1 us.
+  setup_child_handler();
+
+  write(connfd, "Hello, world!", 14);
+
+  close(connfd);
+  return 0;
+}/*}}}*/
+
+void parent_sigint_handler(int signum)/*{{{*/
 {
   UNUSED(signum);
   cleanup_exit(0);
 }/*}}}*/
-void cleanup_exit(int exitn)/*{{{*/
-{
-  if (globals.sockfd != -1)
-    shutdown(globals.sockfd, SHUT_RDWR); // only should fail if our socket
-                                         // is already closed.
-  
-  exit(exitn);
-}/*}}}*/
-void setup_sigint_handler(void)/*{{{*/
+void setup_parent_handler(void)/*{{{*/
 {
   sigset_t empty_set;
   struct sigaction action;
 
   // Capture SIGINT and exit program safely then.
   sigemptyset(&empty_set);
-  action.sa_handler = sigint_handler;
+  action.sa_handler = parent_sigint_handler;
   action.sa_mask = empty_set;
   action.sa_flags = SA_RESTART;
   sigaction(SIGINT, &action, NULL);
+
+  // Ignore SIGUSR1, we will use this to kill our children.
+  action.sa_handler = SIG_IGN;
+  action.sa_mask = empty_set;
+  action.sa_flags = SA_RESTART;
+  sigaction(SIGUSR1, &action, NULL);
 }/*}}}*/
+void cleanup_exit(int exitn)/*{{{*/
+{
+  if (globals.sockfd != -1)
+    shutdown(globals.sockfd, SHUT_RDWR); // only should fail if our socket
+                                         // is already closed.
+
+  kill(0, SIGUSR1); // Kill all our children.
+  // TODO: Maybe wait for our children to actually die.
+  
+  exit(exitn);
+}/*}}}*/
+
+void setup_child_handler(void)/*{{{*/
+{
+  sigset_t empty_set;
+  struct sigaction action;
+
+  // Capture SIGINT and ignore it.
+  sigemptyset(&empty_set);
+  action.sa_handler = SIG_IGN;
+  action.sa_mask = empty_set;
+  action.sa_flags = SA_RESTART;
+  sigaction(SIGINT, &action, NULL);
+
+  // Cleanup on SIGUSR1.
+  action.sa_handler = child_sigusr1_handler;
+  action.sa_mask = empty_set;
+  action.sa_flags = SA_RESTART;
+  sigaction(SIGUSR1, &action, NULL);
+}/*}}}*/
+void child_sigusr1_handler(int signum)/*{{{*/
+{
+  UNUSED(signum);
+  
+  if (globals.connfd != -1)
+    close(globals.connfd);
+
+  exit(0);
+}/*}}}*/
+
